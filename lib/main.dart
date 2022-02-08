8a38c042ff4ae30d4fe55fe84804588d31dbd24d
Marym Gad <mariamgad142@gmail.com>
2022-02-08 17:17:48 +0200

    basics of authentication screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/chat_screen.dart';
-
+import 'screens/auth_screen.dart';
 void main() {
   runApp(MyApp());
 }
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
               primarySwatch: Colors.blue,
             ),
             home: snapshot.connectionState == ConnectionState.done
-                ? ChatScreen()
+                ? AuthScreen()
                 : CircularProgressIndicator(),
           );
         });
